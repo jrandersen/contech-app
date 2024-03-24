8b9033302f90569320fed2bd5cf735037fd485d5
jandersen <andersen.ja@gmail.com>
2024-03-23 20:28:47 -0400

    added search

--- src/components/AppList.js
+++ src/components/AppList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
 import styled from 'styled-components';
 
@@ -10,18 +10,50 @@ const CardContainer = styled.div`
   gap: 30px; /* Add gap between cards */
 `;
 
-const AppList = ({ apps, selectedApp }) => {
+const Input = styled.input`
+  display: block;
+  width: 450px;
+  margin-bottom: 20px;
+  margin-top: 10px;
+  padding: 10px;
+  border: 2px solid #1F3251;
+  border-radius: 5px;
+`;
+
+const AppListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const AppList = ({ apps, selectedApp, position }) => {
+  const [filter, setFilter] = useState('');
+
+  const filteredApps = apps.filter(app => app.name.toLowerCase().includes(filter.toLowerCase()));
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
-    <CardContainer>
-      {apps.map((app) => (
-        <Card
-          key={app.id}
-          app={app}
-          selectedApp={selectedApp}
-          style={{ flexBasis: 'calc(25% - 20px)' }} // Adjust width
-        />
-      ))}
-    </CardContainer>
+    <AppListContainer position={position}>
+      <Input
+        type="text"
+        placeholder="Search by name..."
+        value={filter}
+        onChange={handleFilterChange}
+      />
+      <CardContainer>
+        {filteredApps.map((app) => (
+          <Card
+            key={app.id}
+            app={app}
+            selectedApp={selectedApp}
+            style={{ flexBasis: 'calc(25% - 20px)' }} // Adjust width
+          />
+        ))}
+      </CardContainer>
+    </AppListContainer>
   );
 };
 
